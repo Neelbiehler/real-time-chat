fbbc4082307340a4bf63685f4bab7b98ff438a1e
Neel <neeldev@protonmail.com>
2023-02-17 18:52:31 +0100

    just test

--- main.go
+++ main.go
@@ -1,167 +1,51 @@
 package main
 
 import (
-	"context"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 
-	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
+var addr = flag.String("addr", ":8080", "http service address")
 
-var rdb = redis.NewClient(&redis.Options{
-	Addr: "localhost:6379",
-	DB:   0,
-})
-
-type Client struct {
-	conn      *websocket.Conn
-	send      chan []byte
-	redisConn *redis.PubSub
-}
-
-type Hub struct {
-	clients    map[*Client]bool
-	broadcast  chan []byte
-	register   chan *Client
-	unregister chan *Client
-}
-
-var h = Hub{
-	broadcast:  make(chan []byte),
-	register:   make(chan *Client),
-	unregister: make(chan *Client),
-	clients:    make(map[*Client]bool),
-}
-
-func (h *Hub) run() {
-	for {
-		select {
-		case client := <-h.register:
-			h.clients[client] = true
-		case client := <-h.unregister:
-			if _, ok := h.clients[client]; ok {
-				delete(h.clients, client)
-				close(client.send)
-			}
-		case message := <-h.broadcast:
-			for client := range h.clients {
-				select {
-				case client.send <- message:
-				default:
-					close(client.send)
-					delete(h.clients, client)
-				}
-			}
-		}
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
 	}
-}
-
-func (c *Client) readPump() {
-	ctx := context.Background()
-
-	defer func() {
-		h.unregister <- c
-		c.redisConn.Close()
-		c.conn.Close()
-	}()
-
-	for {
-		_, message, err := c.conn.ReadMessage()
-		if err != nil {
-			break
-		}
-
-		// Publish message to Redis
-		rdb.Publish(ctx, "chat", message)
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
+	http.ServeFile(w, r, "home.html")
 }
 
-func (c *Client) writePump() {
-	defer func() {
-		c.conn.Close()
-	}()
-
-	for {
-		select {
-		case message, ok := <-c.send:
-			if !ok {
-				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
-				return
-			}
-
-			// Write message to WebSocket
-			c.conn.WriteMessage(websocket.TextMessage, message)
-		}
-	}
-}
-
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+func serveWs(hub *hub, w http.ResponseWriter, r *http.Request) {
+	conn, err := websocket.Upgrade(w, r, nil, 1024, 1024)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	if r.Header.Get("Connection") != "Upgrade" {
-		http.Error(w, "Expected 'Connection: Upgrade' header", http.StatusBadRequest)
+		log.Println(err)
 		return
 	}
-
-	if r.Header.Get("Upgrade") != "websocket" {
-		http.Error(w, "Expected 'Upgrade: websocket' header", http.StatusBadRequest)
-		return
-	}
-
-	defer conn.Close()
-
-	redisPubSub := rdb.Subscribe(context.Background(), "chat")
-	defer redisPubSub.Close()
-
-	client := &Client{
-		conn:      conn,
-		send:      make(chan []byte, 256),
-		redisConn: redisPubSub,
-	}
-	h.register <- client
-	defer func() { h.unregister <- client }()
-
-	// Listen for incoming messages from WebSocket
-	go client.readPump()
-
-	// Send messages to WebSocket
-	client.writePump()
-
-	for {
-		msg, err := redisPubSub.Receive(context.Background())
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		switch msg := msg.(type) {
-		case *redis.Message:
-			h.broadcast <- []byte(msg.Payload)
-		}
-	}
+	client := &client{hub: hub, conn: conn, send: make(chan []byte, 256)}
+	client.hub.register <- client
+	go client.write()
+	client.read()
 }
 
 func main() {
-	// Start WebSocket hub
-	go h.run()
-
-	// Serve WebSocket requests
-	http.HandleFunc("/ws", handleConnections)
-	err := http.ListenAndServe(":8080", nil)
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	fmt.Println("Server listening on", *addr)
+	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
-		log.Fatal("Error starting server: ", err)
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
---
+++ websocket.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type message struct {
+	Username string `json:"username"`
+	Text     string `json:"text"`
+}
+
+type hub struct {
+	clients    map[*client]bool
+	broadcast  chan []byte
+	register   chan *client
+	unregister chan *client
+}
+
+type client struct {
+	hub  *hub
+	conn *websocket.Conn
+	send chan []byte
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.clients[client] = true
+		case client := <-h.unregister:
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case message := <-h.broadcast:
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+		}
+	}
+}
+
+func (c *client) read() {
+	defer func() {
+		c.hub.unregister <- c
+		c.conn.Close()
+	}()
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		c.hub.broadcast <- message
+	}
+}
+
+func (c *client) write() {
+	defer func() {
+		c.conn.Close()
+	}()
+	for {
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
+				return
+			}
+			c.conn.WriteMessage(websocket.TextMessage, message)
+		}
+	}
+}
